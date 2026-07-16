f0a1b4007c30c2c193a9f6393184a2a4eaa60871
Adarshs765 <adarshvs1112@gmail.com>
2026-07-16 18:22:24 +0530

    Implement Add Student feature

--- src/student.c
+++ src/student.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "student.h"
 
 void addStudent()
 {
-    printf("\nAdd Student feature coming in Part 3.\n");
+    struct Student s;
+
+    FILE *fp = fopen("data/students.txt", "a");
+
+    if (fp == NULL)
+    {
+        printf("\nError opening file!\n");
+        return;
+    }
+
+    printf("\n========== Add Student ==========\n");
+
+    printf("Enter Student ID: ");
+    scanf("%d", &s.id);
+
+    printf("Enter Student Name: ");
+    scanf(" %[^\n]", s.name);
+
+    printf("Enter Age: ");
+    scanf("%d", &s.age);
+
+    printf("Enter Course: ");
+    scanf(" %[^\n]", s.course);
+
+    fprintf(fp, "%d,%s,%d,%s\n",
+            s.id,
+            s.name,
+            s.age,
+            s.course);
+
+    fclose(fp);
+
+    printf("\nStudent added successfully!\n");
 }
 
 void viewStudents()
